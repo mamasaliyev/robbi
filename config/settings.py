5e5fe030f4e438fe94f4945f40d192a7d8d2090c
Behruz <qosimovsayidazim@gmail.com>
2024-12-07 17:33:29 +0500

    robbi

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-e$prgw^bsi2-18pqtk%9j*3k*s%sqa#jp*jgdlpe$57q0k-g+$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['robbi.pythonanywhere.com']
+ALLOWED_HOSTS = ['robbi.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
